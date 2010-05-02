ea978d7cfd6967b2efbcdebecb4e8e6d8150a7e5
Casey Kolderup <casey@kolderup.org>
2010-05-02 10:32:16 -0400

    now using optparse, added custom credentials file path

--- lucky.rb
+++ lucky.rb
@@ -2,20 +2,31 @@ require 'rubygems'
 require 'net/http'
 require 'uri'
 require 'json'
+require 'optparse'
 
-if ARGV.length == 0
-  puts 'Usage: lucky.rb [query] [query] ...'
-  puts 'Use quotes to make a search query with multiple words.'
-  puts 'Currently requires a file at ~/.google_key with your AJAX API key and domain.'
-  puts 'Custom keyfile location and other output formats forthcoming.'
-  exit
+options = {}
+optparse = OptionParser.new do|opts|
+  opts.banner = "Usage: lucky.rb [options] query1 query2 ..."
+
+  options[:keypath] = '~/.google_key'
+  credentials_text = 'A file that contains your Google API key and referrer URL'
+  opts.on( '--credentials FILE', credentials_text) do|file|
+    options[:keypath] = file
+  end
+
+  opts.on( '-h', '--help', 'Display this screen' ) do
+    puts opts
+    exit
+  end
 end
+
+optparse.parse!
 
 api_url = 'http://ajax.googleapis.com/ajax/services/search/web'
 goog_key = ''
 referrer = ''
 
-File.open(File.expand_path('~/.google_key')) do |auth_info|
+File.open(File.expand_path(options[:keypath])) do |auth_info|
   begin
     goog_key = auth_info.gets
     referrer = auth_info.gets
